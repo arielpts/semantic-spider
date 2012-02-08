d59cef16a5e94356393a7f50d2e9e1430f7d0941
Ariel Patschiki <arielpts@me.com>
2012-02-08 15:07:31 -0200

    First commit

---
+++ semantic-spider.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'anemone'
+require 'pry'
+require 'mysql'
+
+class Anemone::Page 
+  def in_domain?(uri)
+    same_domain = uri.host == @url.host
+
+    if (!same_domain && (uri.host == 'www.midiasemmascara.org' or uri.host == 'www.midiasemmascara.com.br' or uri.host == 'midiasemmascara.org'))
+
+      external_url = {
+       referer: {
+         host: self.url.host, path: self.url.path, url: self.url.to_s
+       },
+       host: uri.host,
+       path: uri.path,
+       url: uri.to_s
+      }
+      
+      links = db['links']
+      links.insert external_url
+    end
+
+    same_domain
+  end
+end
+
+Não deveria estar aqui
+db = Mongo::Connection.new.db('anemone')
+links = db['links_sum']
+
+links.find.each do |f|
+  if (!black_list.include? f['links_sum.host'])
+    uri = 'http://' + f['links_sum.host']
+    puts uri
+    # Primeira Passada
+    #    crawl = Anemone.crawl("http://www.midiasemmascara.org/", {
+    # Segunda Passada
+      crawl = Anemone.crawl(uri, {
+        :depth_limit => 2,
+        :threads => 10,
+        :discard_page_bodies => true
+      }) do |anemone|
+      anemone.storage = Anemone::Storage.MongoDB
+      anemone.on_every_page do |page|
+        title = page.doc.at('title').inner_html rescue nil
+        puts "#{title} (#{page.depth})"
+      end
+    end
+  end
+end
+
+puts "EOF"
